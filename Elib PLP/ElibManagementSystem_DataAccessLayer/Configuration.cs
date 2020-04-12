49d1ac2e56fcff7621b9bea3d7e224ede5f55c02
akhilkusuma0502 <59748844+akhilkusuma0502@users.noreply.github.com>
2020-04-12 17:18:34 -0400

    Create Configuration.cs

---
+++ Elib PLP/ElibManagementSystem_DataAccessLayer/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElibManagementSystem_DataAccessLayer
+{
+    using System.Configuration;
+    static class Configuration
+    {
+        static string _connectionString, _providerName;
+        static Configuration()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["ELIB_Management_System"].ConnectionString;
+            _providerName = ConfigurationManager.ConnectionStrings["ELIB_Management_System"].ProviderName;
+        }
+        public static string ConnectionString { get { return _connectionString; } }
+        public static string ProviderName { get { return _providerName; } }
+
+    }
+}
