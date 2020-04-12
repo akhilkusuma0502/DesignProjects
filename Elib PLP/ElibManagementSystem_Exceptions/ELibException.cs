3b697170b752f7807643700226ee8dd26082ede9
akhilkusuma0502 <59748844+akhilkusuma0502@users.noreply.github.com>
2020-04-12 17:20:23 -0400

    Create ELibException.cs

---
+++ Elib PLP/ElibManagementSystem_Exceptions/ELibException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElibManagementSystem_Exceptions
+{
+    public class ELibException : ApplicationException
+    {
+        public ELibException()
+        {
+        }
+
+        public ELibException(string message) : base(message)
+        {
+        }
+
+        public ELibException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
