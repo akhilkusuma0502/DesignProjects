b6091c925dec529682dbbc089bcd3096f0757af5
akhilkusuma0502 <59748844+akhilkusuma0502@users.noreply.github.com>
2020-04-12 17:19:01 -0400

    Create Disciplines.cs

---
+++ Elib PLP/ElibManagementSystem_Entities/Disciplines.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElibManagementSystem_Entities
+{
+    public class Disciplines
+    {
+        private int _disciplineId;
+
+        public int DisciplineId
+        {
+            get { return _disciplineId; }
+            set { _disciplineId = value; }
+        }
+
+        private string _disciplineName;
+
+        public string DisciplineName
+        {
+            get { return _disciplineName; }
+            set { _disciplineName = value; }
+        }
+
+        public Disciplines()
+        {
+
+        }
+
+        public Disciplines(int DisciplineId, string DisciplineName)
+        {
+            this.DisciplineId = DisciplineId;
+            this.DisciplineName = DisciplineName;
+        }
+
+    }
+}
