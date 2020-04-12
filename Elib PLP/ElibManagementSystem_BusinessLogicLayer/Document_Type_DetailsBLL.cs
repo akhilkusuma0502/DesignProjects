625007e9b7edbd3f4d40f6389aa2fc1fb39afc34
akhilkusuma0502 <59748844+akhilkusuma0502@users.noreply.github.com>
2020-04-12 17:18:26 -0400

    Create Document_Type_DetailsBLL.cs

---
+++ Elib PLP/ElibManagementSystem_BusinessLogicLayer/Document_Type_DetailsBLL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElibManagementSystem_BusinessLogicLayer
+{
+    using ElibManagementSystem_Entities;
+    using ElibManagementSystem_Exceptions;
+    using ElibManagementSystem_DataAccessLayer;
+    public  class Document_Type_DetailsBLL
+    {
+        Document_Type_DetailsOperations DocumentTypeDetailsDALObj;
+        public Document_Type_DetailsBLL()
+        {
+            DocumentTypeDetailsDALObj = new Document_Type_DetailsOperations();
+        }
+        public List<Document_Type_Details> GetAll()
+        {
+            var DocumentTypeDetailsList = new List<Document_Type_Details>();
+            try
+            {
+                var ObjDAL = new Document_Type_DetailsOperations();
+                DocumentTypeDetailsList = ObjDAL.GetDocumentTypeDetails();
+                if (DocumentTypeDetailsList == null || DocumentTypeDetailsList.Count == 0)
+                    throw new ELibException("Document Type Details not found");
+            }
+            catch (ELibException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new ELibException("Unknown error", ex);
+            }
+            return DocumentTypeDetailsList;
+        }
+    }
+}
