224d1fb24cee1194696e10d694ee636c7f11b365
akhilkusuma0502 <59748844+akhilkusuma0502@users.noreply.github.com>
2020-04-12 17:19:04 -0400

    Create Document_Type_Details.cs

---
+++ Elib PLP/ElibManagementSystem_Entities/Document_Type_Details.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElibManagementSystem_Entities
+{
+   public class Document_Type_Details
+    {
+
+        private int _documentTypeId;
+
+        public int DocumentTypeId
+        {
+            get { return _documentTypeId; }
+            set { _documentTypeId = value; }
+        }
+        private string _documentTypeName;
+
+        public string DocumentTypeName
+        {
+            get { return _documentTypeName; }
+            set { _documentTypeName = value; }
+        }
+        public Document_Type_Details()
+        {
+
+        }
+
+        public Document_Type_Details(int DocumentTypeId, string DocumentTypeName)
+        {
+            this.DocumentTypeId = DocumentTypeId;
+            this.DocumentTypeName = DocumentTypeName;
+        }
+    }
+}
